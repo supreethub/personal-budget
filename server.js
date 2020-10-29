978a452191c7b63ff0b29544e4933020b3a62f6e
Supreeth <supreethub96@gmail.com>
2020-10-29 18:50:56 -0400

    Revert "Added mongoose schema model"
    This reverts commit 59047008b9acdb27bd5797865fcfed76588e6e6c.
    
    Commit revert - pushing to a different repo

--- server.js
+++ server.js
@@ -1,31 +1,16 @@
 const express = require('express');
-//const movedData = require('./movedData.json');
+const movedData = require('./movedData.json');
 const app = express();
 const cors = require('cors');
 const port = 3000;
 
-
-const mongoose = require("mongoose");
-const pbModel = require('./schema_model/mongo_schema');
-
-var url = 'mongodb://localhost:27017/personal_budget_db';
-
-
 app.use(cors());
 
 
-// app.get('/budget', (req,res) => {
-//     res.json(movedData)
-// });
+app.get('/budget', (req,res) => {
+    res.json(movedData)
+});
 
-mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => {
-        app.listen(port, () => {
-            console.log(`API listening at http://localhost:${port}`);
-        })
-        .catch((connectionErr) => {
-            console.log(connectionErr);
-            mongoose.connection.close();
-        })
-    })
-
+app.listen(port, () => {
+    console.log('API listening at http://localhost:${port}');
+});
